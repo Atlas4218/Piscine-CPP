edc5cdd462b71654b2c681433e2dad24b74f87c2
Roman ASTIE <roman.astie@wanadoo.fr>
2024-02-05 18:10:38 +0100

    cpp06

---
+++ Cpp06/ScalarConverter.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rastie <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/05 18:01:04 by rastie            #+#    #+#             */
+/*   Updated: 2024/02/05 18:09:25 by rastie           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+# include <string>
+# include <iostream>
+
+class ScalarConverter
+{
+private:
+
+
+public:
+	static void convert(std::string litteral);
+};
+#endif
