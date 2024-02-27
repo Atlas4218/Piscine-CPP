48d1c75c797ca5bbba914c3190bb1ed500f99e52
Roman ASTIE <roman.astie@wanadoo.fr>
2024-02-27 17:41:16 +0100

    Cpp07

--- Cpp07/ex02/Array.hpp
+++ Cpp07/ex02/Array.hpp
@@ -1,12 +1,23 @@
+#include <stdexcept>
 template<typename T>
 class Array
 {
 private:
-    T *element
+    T *_element;
+    unsigned int _size;
 public:
-    Array(void);
-    Array(unsigned int n);
-    Array(Array<T> array);
-    Array<T> &operator=(Array<T> &rhs);
-    ~Array();
+    Array(void): _element(0) {};
+    Array(unsigned int n): _size(n) {_element = new T [n];}
+    Array<T> &operator=(Array<T> &rhs) {if (_element)
+                                            delete[] _element;
+                                        _element = new T[rhs._size];
+                                        for (unsigned int i = 0; i < rhs._size; i++)
+                                            _element[i] = rhs._element[i];
+                                        return (*this);}
+    Array(Array<T> &array) {*this = array;}
+    ~Array() {delete[] _element;}
+    T& operator[](std::size_t idx) {if(idx < 0 || idx >= _size)
+                                        throw (std::out_of_range("Index out of bounds"));
+                                    return  (_element[idx]);}
+    unsigned int size() {return (_size);}
 };
