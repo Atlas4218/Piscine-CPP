530ef5f196d522736c90bc42e47e00bc532b2a52
Roman ASTIE <roman.astie@wanadoo.fr>
2024-03-02 17:28:33 +0100

    cpp08

--- Cpp08/ex02/MutantStack.hpp
+++ Cpp08/ex02/MutantStack.hpp
@@ -1,22 +1,34 @@
-template<typename T>
-class MutantStack
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rastie <rastie@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/02 17:02:21 by rastie            #+#    #+#             */
+/*   Updated: 2024/03/02 17:27:18 by rastie           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stack>
+#include <deque>
+#include <iostream>
+
+
+template<typename T, class Container = std::deque<T>>
+class MutantStack: public std::stack<T, Container>
 {
 private:
     /* data */
 public:
-    MutantStack(void);
-    MutantStack(const MutantStack &stack);
-    ~MutantStack();
-    MutantStack &operator=(const MutantStack &rhs);
-
-    T top();
-
-    bool empty();
-    size_t size();
-
-    void push(T element);
-    void pop();
-
-    iterator begin();
-    iterator end();
+    MutantStack(void) {}
+    MutantStack(const MutantStack &stack) {*this = rhs;}
+    ~MutantStack() {}
+    MutantStack &operator=(const MutantStack &rhs) {
+        std::stack<T, Container>::operator=(rhs);
+        return *this;
+    }
+    typedef typename Container::iterator iterator;
+    iterator begin() {return this->c.begin()}
+    iterator end() {return this->c.end()}
 };
